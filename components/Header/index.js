acb2926cdee1a011cd410ad3ed87e303578ed5e0
boogiepop-135 <levi.eduardo2024@gmail.com>
2025-04-20 23:56:24 +0000

    cv listo

--- components/Header/index.js
+++ components/Header/index.js
@@ -77,7 +77,7 @@ const Header = ({ handleWorkScroll, handleAboutScroll, isBlog }) => {
                   {showResume && (
                     <Button
                       onClick={() =>
-                        window.open("mailto:hello@chetanverma.com")
+                        window.open("mailto:levi.eduardo2024@gmail.com")
                       }
                     >
                       Resume
@@ -85,7 +85,9 @@ const Header = ({ handleWorkScroll, handleAboutScroll, isBlog }) => {
                   )}
 
                   <Button
-                    onClick={() => window.open("mailto:hello@chetanverma.com")}
+                    onClick={() =>
+                      window.open("mailto:levi.eduardo2024@gmail.com")
+                    }
                   >
                     Contact
                   </Button>
@@ -108,7 +110,9 @@ const Header = ({ handleWorkScroll, handleAboutScroll, isBlog }) => {
                   )}
 
                   <Button
-                    onClick={() => window.open("mailto:hello@chetanverma.com")}
+                    onClick={() =>
+                      window.open("mailto:levi.eduardo2024@gmail.com")
+                    }
                   >
                     Contact
                   </Button>
@@ -145,7 +149,11 @@ const Header = ({ handleWorkScroll, handleAboutScroll, isBlog }) => {
               </Button>
             )}
 
-            <Button onClick={() => window.open("mailto:hello@chetanverma.com")}>
+            <Button
+              onClick={() =>
+                window.open("mailto:levi.eduardo2024@gmail.com")
+              }
+            >
               Contact
             </Button>
             {mounted && theme && data.darkMode && (
@@ -174,7 +182,11 @@ const Header = ({ handleWorkScroll, handleAboutScroll, isBlog }) => {
               </Button>
             )}
 
-            <Button onClick={() => window.open("mailto:hello@chetanverma.com")}>
+            <Button
+              onClick={() =>
+                window.open("mailto:levi.eduardo2024@gmail.com")
+              }
+            >
               Contact
             </Button>
 
